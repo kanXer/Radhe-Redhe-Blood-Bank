eb2bafabbe3698828a901ea988b87b775cf64aa2
Sahil Lala <user.kanxer@gmail.com>
2026-08-22 23:24:07 +0530

    Update allowedDevOrigins in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ["192.168.29.41", "localhost:3000"],
+  allowedDevOrigins: ["radhe-radhe-blood-bank.vercel.app"],
 };
 
 export default nextConfig;

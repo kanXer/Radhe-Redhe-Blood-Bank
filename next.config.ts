1c6e01baee8623a20add66f2a9d11846f7363d1e
Sahil Lala <user.kanxer@gmail.com>
2026-08-26 09:00:27 +0530

    Add new allowedDevOrigin for development

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ["radhe-radhe-blood-bank.vercel.app"],
+  allowedDevOrigins: ["radhe-radhe-blood-bank.vercel.app","radheyradheybloodcentre.in"],
 };
 
 export default nextConfig;
